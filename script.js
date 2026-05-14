35f82cdb859c2fb5ee8fa9d46c007161c5d399f7
LouixFelps <109196136+LouixFelps@users.noreply.github.com>
2026-05-13 21:17:05 -0300

    Create Rock Paper Scissors Game.
    For this game, was needed to create functions that make the code works.
    
    All created functions:
    
        playConfirm()
        --asks the user if it wants to play the game with prompt, if yes
        than code leads to next function;
    
        initializeGame()
        --if playConfirm result true, than it will start the game asking
        player choose between rock, paper or scissor, than the result is
        passed to next function;
    
        rockPaperScissor()
        --format the user choice to always make the first letter upper case,
        --make sure to user put only asked choice using if condition,
        --if everything is ok, it stores a result from machine using
        generateMachineResult function,
        --compare the user result with machine result using compareResult
        function,
        --than put the result into confirmVictory function;
    
        generateMachineResult()
        --generates a random number between 1 and 100
        --if the result is less than 33, returns "Paper",
        --if the result is greater than 66, returns "Rock",
        -- and if result is none of above conditions, returns "Scissor";
    
        compareResult()
        --returns true if the user choice wins against machine result,
        --returns false if the user choice loses against machine result,
        --and if it is a draw, returns an alert confirming the draw and
        calls playConfirm function;
    
        confirmVictory()
        --if result is true, than return a win message and calls playConfirm
        function,
        if result is false, than return a loss message and calls playConfirm
        function;

--- script.js
+++ script.js
@@ -1,28 +1,84 @@
 
 
 
-//Usar o prompt para verificar se o usuario quer iniciar o jogo
-let jogar = confirm("Você quer jogar?", "");
-iniciarJogo(jogar);
 
 
 
-//Criar uma função onde pedra, papel e tesoura são geradas aleatoriamente e retornar uma dessas
-//Comparar resultado da função com o do usuário
-//Caso usuario ganhe, retornar verdadeiro com uma mensagem de parabens e clicar para jogar novamente
-//Caso o usuario perca, retornar falso, e com uma mensagem para jogar novamente
 
 
 
-function iniciarJogo(jogar){
-    if(jogar){
-        let escolha = prompt("Pedra, Papel ou Tesoura?", "") //Perguntar se ele escolhe pedra, papel ou tesoura
-        pedraPapelTesoura(escolha);
+
+
+
+
+playConfirm() //Usar o prompt para verificar se o usuario quer iniciar o jogo
+
+function initializeGame(){
+    let playerResult = prompt("Rock, Paper or Scissors?", "") //Perguntar se ele escolhe pedra, papel ou tesoura
+    rockPaperScissor(playerResult);
+
+}
+
+function rockPaperScissor(playerResult){
+    let userResult = playerResult.replace(playerResult[0], playerResult[0].toUpperCase())
+    if(userResult != "Paper" && userResult != "Rock" && userResult != "Scissor"){
+        return rockPaperScissor(prompt("Choose Between Rock, Paper or Scissors Only!", ""))
     }else{
-        console.log("não");
+        let machineResult = generateMachineResult();
+        let lastResult = compareResult(userResult, machineResult) //Caso usuario ganhe, retornar verdadeiro com uma mensagem de parabens e clicar para jogar novamente
+        //Caso o usuario perca, retornar falso, e com uma mensagem para jogar novamente
+        confirmVictory(lastResult)
     }
 }
 
-function pedraPapelTesoura(escolha){
-    let resultadoMaquina = gerarResultado();
+
+function generateMachineResult(){
+    let result = Math.floor(Math.random() * 100) + 1 //Criar uma função onde pedra, papel e tesoura são geradas aleatoriamente e retornar uma dessas
+    console.log(result)
+    
+    if(result < 33){
+        return "Paper"
+    }else if(result > 66){
+        return "Rock"
+    }else{
+        return "Scissor"
+    }
+}
+
+function compareResult(userResult, machineResult){ //Comparar resultado da função com o do usuário
+    if(userResult == machineResult){
+        alert("It's a draw!")
+        return playConfirm()
+    }else if(userResult === "Paper" && machineResult === "Rock"){
+        return true
+    }else if(userResult === "Paper" && machineResult === "Scissor"){
+        return false
+    }else if(userResult === "Rock" && machineResult === "Scissor"){
+        return true
+    }else if(userResult === "Rock" && machineResult === "Paper"){
+        return false
+    }else if(userResult === "Scissor" && machineResult == "Paper"){
+        return true
+    }else if(userResult === "Scissor" && machineResult === "Rock"){
+        return false
+    }
+}
+
+function playConfirm(){
+    let jogar = confirm("Você quer jogar?", "");
+    if(jogar){
+    initializeGame(jogar);
+    }else{
+        alert("Reload page if you want to play!")
+    }
+}
+
+function confirmVictory(value){ //value will be a boolean 
+    if(value === true){
+        alert("You Win The Game!!!")
+        playConfirm()
+    }else if(value === false){
+        alert("You Lost!");
+        playConfirm()
+    }
 }
